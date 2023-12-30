63fee3f91ff5a773f44545b6e0c4a2decee1d0a9
Bruno1077 <141870983+Bruno1077@users.noreply.github.com>
2023-12-30 06:08:36 -0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# introduction_to_python
+this respitory contains variables, data types control row statements in python
---
+++ python_odd_or_even.py
@@ -0,0 +1,8 @@
+# python program to check if the input number is odd or even
+# a number is even if division by 2 gives a remainder of 0
+# if the remainder is 1 then it is an odd number
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+   print("{0} is Even".format(num))
+else:
+   print("{0} is Odd".format(num))
